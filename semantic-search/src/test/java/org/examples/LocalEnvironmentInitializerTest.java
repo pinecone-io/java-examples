f89c4f8ef8ef27102ff30df7c3090dba4817a0df
aulorbe <aulorbe@gmail.com>
2024-03-29 14:24:14 -0700

    Refactor LocalEnvironmentInitializer to be simpler + add tests

---
+++ semantic-search/src/test/java/org/examples/LocalEnvironmentInitializerTest.java
@@ -0,0 +1,40 @@
+package org.examples;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LocalEnvironmentInitializerTest {
+
+    @Test
+    public void testCheckAndSetEnvVars() {
+        String fakePineconeApiKey = "fakePineconeApiKey";
+        String fakeOpenAIApiKey = "fakeOpenAiApiKey";
+
+        LocalEnvironInitializer initializer = new LocalEnvironInitializer(fakePineconeApiKey, fakeOpenAIApiKey);
+        assertEquals(initializer.getPineconeApiKey(), fakePineconeApiKey);
+        assertEquals(initializer.getOpenAiApiKey(), fakeOpenAIApiKey);
+
+        IllegalStateException pineconeEmptyStringException = assertThrows(IllegalStateException.class, () -> {
+            new LocalEnvironInitializer("", fakeOpenAIApiKey);
+        });
+        assertEquals("Mandatory environment variable PINECONE_API_KEY is not set", pineconeEmptyStringException.getMessage());
+
+        IllegalStateException pineconeNullException = assertThrows(IllegalStateException.class, () -> {
+            new LocalEnvironInitializer(null, fakeOpenAIApiKey);
+        });
+        assertEquals("Mandatory environment variable PINECONE_API_KEY is not set", pineconeNullException.getMessage());
+
+        IllegalStateException openAIEmptyStringException = assertThrows(IllegalStateException.class, () -> {
+            new LocalEnvironInitializer(fakePineconeApiKey, "");
+        });
+        assertEquals("Mandatory environment variable OPENAI_API_KEY is not set", openAIEmptyStringException.getMessage());
+
+        IllegalStateException openAINullException = assertThrows(IllegalStateException.class, () -> {
+            new LocalEnvironInitializer(fakePineconeApiKey, null);
+        });
+        assertEquals("Mandatory environment variable OPENAI_API_KEY is not set", openAINullException.getMessage());
+    }
+
+}
